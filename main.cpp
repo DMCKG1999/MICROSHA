0276c6d461718e066d2bfc6606d6032eca763626
Dmitry <dmckg1999@gmail.com>
2018-10-18 01:50:22 +0300

    Fix problems.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
 
 // Name = print current directory name
 void pcdn (bool pr);
-
 
 int main (int argc, char **argv, char **envp) {
 
     pcdn(true);
 
+
+    printf ("\n");
     return 0;
 }
 
 // Name = print current directory name
 void pcdn (bool pr) {
-    printf (“%s%c”, get_current_dir_name (), (pr)? '!' : '>');
+    printf ("%s%c ", get_current_dir_name (), (pr)? '!' : '>');
 }
